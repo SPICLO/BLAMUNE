85ffce99c86cfef16108c07e8f9efd2f01c37941
BLAMUNE <blamune@project.com>
2026-09-25 06:21:21 -0400

    chore: cache v3 (rampe serpent)

--- site/sw.js
+++ site/sw.js
@@ -1,5 +1,5 @@
 // BLAMUNE Service Worker - offline cache
-const CACHE = 'blamune-v2';
+const CACHE = 'blamune-v3';
 const ASSETS = ['/', '/index.html', '/style.css', '/app.js', '/manifest.json', '/logo.png'];
 
 self.addEventListener('install', (e) => {
